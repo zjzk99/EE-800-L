6b613bd9fc60663a5e96a40ffe9590a50aaaed95
kai.eric.zhang@gmail.com <kai.eric.zhang@gmail.com>
2018-11-04 23:12:47 -0500

    remove negligible files

--- Realtime_Multi-Person_Pose_Estimation/training/get_lmdb.sh
+++
@@ -1,3 +0,0 @@
-mkdir lmdb_trainVal
-wget -nc --directory-prefix=lmdb_trainVal/ 		http://posefs1.perception.cs.cmu.edu/Users/ZheCao/lmdb_trainVal/data.mdb
-wget -nc --directory-prefix=lmdb_trainVal/ 		http://posefs1.perception.cs.cmu.edu/Users/ZheCao/lmdb_trainVal/lock.mdb
